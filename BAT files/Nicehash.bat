59ae781ac6333ceb8ef51e37244dd2fd87fc5874
tmattel <77650997+tmattel@users.noreply.github.com>
2021-05-01 15:15:24 -0700

    Update Nicehash.bat

--- BAT files/Nicehash.bat
+++ BAT files/Nicehash.bat
@@ -1,2 +1,2 @@
-EthDcrMiner64.exe -epool daggerhashimoto.eu.nicehash.com:3353 -ewal 1Pz3EcHsRuJkraNRACUrBrLaxiTPP1VHJZ -epsw x -dbg -1 -retrydelay 1 -ftime 55 -tt 79 -ttli 77 -tstop 89 -tstart 85 -fanmin 30 -r 0 -esm 3 -erate 1 -allcoins 1 -allpools 1
+EthDcrMiner64.exe -epool daggerhashimoto.usa-west.nicehash.com:3353 -ewal 3Q5dYCsmck51jcaG4iJM1x3EKZbQ6hxqcN -epsw x -dbg -1 -retrydelay 1 -ftime 55 -tt 79 -ttli 77 -tstop 89 -tstart 85 -fanmin 30 -r 0 -esm 3 -erate 1 -allcoins 1 -allpools 1
 pause
