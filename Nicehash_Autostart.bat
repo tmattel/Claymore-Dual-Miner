a23483eebc85446838ec817689d50d988e8a2565
tmattel <77650997+tmattel@users.noreply.github.com>
2021-05-01 21:15:49 -0700

    Create Nicehash_Autostart.bat

---
+++ Nicehash_Autostart.bat
@@ -0,0 +1,3 @@
+:start
+EthDcrMiner64.exe -epool daggerhashimoto.usa-west.nicehash.com:3353 -ewal 3Q5dYCsmck51jcaG4iJM1x3EKZbQ6hxqcN -epsw x -dbg -1 -retrydelay 1 -ftime 55 -tt 79 -ttli 77 -tstop 89 -tstart 85 -fanmin 30 -r 0 -esm 3 -erate 1 -allcoins 1 -allpools 1
+goto start
